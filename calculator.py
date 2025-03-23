24c5f1a8e9c9ef8d8f4eacbc7f4d791ad953d650
Mathias-Ali <alimathiasdenen@gmail.com>
2025-03-23 19:55:04 +0100

    calculator assignment

---
+++ calculator.py
@@ -0,0 +1,15 @@
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter a second number:"))
+
+# addition of two numbers
+answer = number_1 + number_2
+print("The result of addition is "+str(answer)+"")
+
+# multiplication of two numbers
+answer = number_1 * number_2
+print("The result of multiplication is "+str(answer)+"")
+
+# subtraction of two numbers
+answer = number_1 - number_2
+print("The result of subtraction is "+str(answer)+"")
+
